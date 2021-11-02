1844e738fa30f1adc0a20ee1811af32dcc5bcd18
Brian O'Meara <omeara.brian@gmail.com>
2021-11-01 21:50:45 -0400

    updating site

--- _targets.R
+++ _targets.R
@@ -20,12 +20,7 @@ tar_option_set(packages = "dplyr")
 
 # End this file with a list of target objects.
 list(
-  tar_invalidate("cdc_weekly_metadata"),
   tar_target(hesitancy_by_county, GetHesitancyByCounty()),
-  #tar_target(cases_by_county, GetCountyCaseAverages()),
-  #tar_target(countycitystate, GetCountyCityState()),
-  #tar_target(hospitalization_by_county, GetHospitalData(countycitystate)),
-  #tar_target(aggregation_by_county, AggregateCountyInformation(hesitancy_by_county, cases_by_county, hospitalization_by_county))
   tar_target(cdc_weekly_metadata, GetCommunityTransmissionReportLastDate()),
   tar_target(cdc_weekly, GetCommunityTransmissionReport(report_url=cdc_weekly_metadata$report_url)),
   tar_target(cdc_all_metadata, GetAllCommunityTransmissionReportDates()),
@@ -33,8 +28,9 @@ list(
   tar_target(aggregation_by_county, JoinHesitancyWithCDCWeekly(hesitancy_by_county, cdc_weekly)),
   tar_target(aggregation_table_csv, WriteCSVTable(aggregation_by_county), format="file"),
   tar_target(geonames_sites, GetGeonames()),
-  tar_target(bia_sites, GetBIA_data()),
-  tar_target(all_sites, MergeBIAWithGeoNames(geonames_sites, bia_sites)),
+  #tar_target(bia_sites, GetBIA_data()),
+  #tar_target(all_sites, MergeBIAWithGeoNames(geonames_sites, bia_sites)),
+  tar_target(all_sites, geonames_sites), #until dataset is available again
   tar_target(sites_and_covid, MergeAllSitesWithCovidInfo(all_sites, aggregation_by_county)),
   tar_target(site_info_formatted, FormatSitesAndCovid(sites_and_covid)),
   tar_target(site_info_formatted_csv, WriteCSVTable(site_info_formatted, file_out="data/site_info.csv"), format="file"),
@@ -43,15 +39,9 @@ list(
   tar_target(all_time_series, GetAllTimeSeries(cdc_all_reports)),
   tar_render(report, "index.Rmd", params = list(attractions_table = FormatForDT(site_info_formatted, cities=FALSE), cities_table = FormatForDT(site_info_formatted, cities=TRUE), date_community=cdc_weekly_metadata$report_date, all_predictions=all_predictions, all_time_series=all_time_series)),
   tar_render(animations, "animations.Rmd", params = list(all_predictions=all_predictions)),
-  tar_target(county_pages, RenderCountyPages(site_info_formatted, nationwide_info, aggregation_by_county, date_community=cdc_weekly_metadata$report_date, all_predictions=all_predictions, all_time_series=all_time_series)),
   tar_target(all_time_series_saved, write.csv(all_time_series, file=gzfile("data/all_time_series.csv.gz"))),
   tar_target(site_info_formatted_saved, write.csv(site_info_formatted, file=gzfile("data/site_info_formatted.csv.gz"))),
   tar_target(nationwide_info_saved, write.csv(nationwide_info, file=gzfile("data/nationwide_info.csv.gz"))),
   tar_target(aggregation_by_county_saved, write.csv(aggregation_by_county, file=gzfile("data/aggregation_by_county.csv.gz"))),
-  tar_target(cdc_all_reports_saved, write.csv(cdc_all_reports, file=gzfile("data/cdc_all_reports.csv.gz"))),
-  tar_invalidate("report"),
-  #tar_invalidate("county_pages"),
-  tar_invalidate("cdc_weekly_metadata"),
-  tar_invalidate("cdc_all_metadata"),
-  tar_invalidate("hesitancy_by_county")
+  tar_target(cdc_all_reports_saved, write.csv(cdc_all_reports, file=gzfile("data/cdc_all_reports.csv.gz")))
 )
